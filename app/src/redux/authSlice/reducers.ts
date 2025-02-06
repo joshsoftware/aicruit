7edc70e9cff7ed25faa7107b985bb30f99a69f7a
RohanDhalpe05 <rohan.dhalpe@joshsoftware.com>
2025-02-06 11:01:46 +0530

    Updated redux setup

--- app/src/redux/authSlice/reducers.ts
+++ app/src/redux/authSlice/reducers.ts
@@ -1,20 +1,24 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { UserAuthData, AuthState } from "./types";
+import { AuthState, UserAuthData } from "./types";
+import LocalStorage from "@/utils/localStore";
 
 export function loadAuthReducer(
   state: AuthState,
-  action: PayloadAction<{
-    token: string;
-    user: UserAuthData;
-  }>
+  action: PayloadAction<{ token: string; user: UserAuthData }>
 ) {
   const { token, user } = action.payload;
-
   state.token = token;
   state.user = user;
+
+  LocalStorage.setItem(
+    LocalStorage.AUTH_USER_DATA,
+    JSON.stringify({ token, user })
+  );
 }
 
 export function resetAuthReducer(state: AuthState) {
   state.token = null;
   state.user = null;
+
+  LocalStorage.removeItem(LocalStorage.AUTH_USER_DATA);
 }
